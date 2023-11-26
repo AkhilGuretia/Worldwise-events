aee7da2813a68fbab39aedd794835d52139fcf88
Akhil Kumar <akhilguretia@gmail.com>
2023-11-26 13:59:10 +0530

    create markup for the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,24 @@
 import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
 const Homepage = () => {
   return (
-    <div>
+    <main className={styles.homepage}>
       <PageNav />
-      <h2>Homepage</h2>
-    </div>
+      <section>
+        <h1>
+          You want to host events and collaborate with the world.
+          <br />
+          WorldWise Events helps you reach the right audience.
+        </h1>
+        <h2>
+          Events bringing together the right ingredients to generate value for
+          the target audience. Over 2 million professional events are hosted for
+          various topic, attracting over a billion knowledge audience around the
+          world. We aim to ensure these events reach the right audience.
+        </h2>
+      </section>
+    </main>
   );
 };
 
